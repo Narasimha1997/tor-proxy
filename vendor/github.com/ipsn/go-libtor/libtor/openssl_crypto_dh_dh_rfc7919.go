543bf5023bdcc38b7673fb08e4992de1dd01cb49
Narasimha1997 <yogaimpression@gmail.com>
2021-09-24 13:43:05 +0530

    Added vendoring

---
+++ vendor/github.com/ipsn/go-libtor/libtor/openssl_crypto_dh_dh_rfc7919.go
@@ -0,0 +1,13 @@
+// go-libtor - Self-contained Tor from Go
+// Copyright (c) 2018 Péter Szilágyi. All rights reserved.
+
+package libtor
+
+/*
+#define DSO_NONE
+#define OPENSSLDIR "/usr/local/ssl"
+#define ENGINESDIR "/usr/local/lib/engines"
+
+#include <../crypto/dh/dh_rfc7919.c>
+*/
+import "C"
---
+++ vendor/github.com/ipsn/go-libtor/libtor/openssl_crypto_ec_curve448_eddsa.go
@@ -0,0 +1,13 @@
+// go-libtor - Self-contained Tor from Go
+// Copyright (c) 2018 Péter Szilágyi. All rights reserved.
+
+package libtor
+
+/*
+#define DSO_NONE
+#define OPENSSLDIR "/usr/local/ssl"
+#define ENGINESDIR "/usr/local/lib/engines"
+
+#include <../crypto/ec/curve448/eddsa.c>
+*/
+import "C"
